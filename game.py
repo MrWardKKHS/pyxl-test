4f28ffa0abd9211e0c1dd4bfdd1ddbdfd5e5deeb
Alex Ward <AWard@kerikerihigh.ac.nz>
2022-10-27 12:50:50 +1300

    looping square

--- game.py
+++ game.py
@@ -1,13 +1,23 @@
 import pyxel
 
-pyxel.init(160, 120)
+WIDTH = 200
+HEIGHT = 200
+class Game:
+    def __init__(self) -> None:
+        pyxel.init(WIDTH, HEIGHT)
+        self.x = 0
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        self.x += 1
+        if self.x > WIDTH:
+            self.x = -20
 
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10, 10, 20, 20, 11)
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 10, 20, 20, 11)
 
-pyxel.run(update, draw)
+
+game = Game()
+pyxel.run(game.update, game.draw)
