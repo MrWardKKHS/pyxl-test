b7c7f2e5c480a619e601eccaf9d6f9473416f89e
Alex Ward <AWard@kerikerihigh.ac.nz>
2022-10-27 14:16:38 +1300

    made a little square

--- fresh.py
+++ fresh.py
@@ -2,16 +2,17 @@ import pyxel
 
 WIDTH = 200
 HEIGHT = 200
-pyxel.init(WIDTH, HEIGHT)
 x = 0
+pyxel.init(WIDTH, HEIGHT)
 
 def update():
+    global x
     x += 5
     if x > WIDTH:
         x = -20
 
 def draw():
     pyxel.cls(0)
-    pyxel.rect(x, 10, 20, 20, 11)
+    pyxel.rect(x, 10, 20, 20, 10)
 
 pyxel.run(update, draw)
